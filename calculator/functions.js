e9c808f7c1d1e5a71a6576ff0d0568f159291ac2
JamieVaughn <wxv6@case.edu>
2019-12-12 02:00:39 -0500

    Add calc solution branch

--- calculator/functions.js
+++ calculator/functions.js
@@ -4,33 +4,53 @@
 
 
 // Write a function to add two numbers
-
-
-
+function add(num1, num2) {
+    return Number(num1) + Number(num2)
+}
 // Write a function to subtract two numbers
-
-
-
+function subtract(num1, num2) {
+    return num1 - num2
+  }
 // Write a function to multiply two numbers
-
-
-
+function multiply(num1, num2) {
+    return num1 * num2
+  }
 // Write a function to divide two numbers
-
-
-
-
+function divide(num1, num2) {
+    return num1 / num2
+  }
 // Write a function that accepts a string representing 
 // an operation and two numbers 
 // and returns a numerical result 
 // (use your functions from above within this function)
-function equalsHandler(operation, num1, num2){
-
-
-    return answer;
+function equalsHandler(op, first, second){
+    switch(op) {
+        case 'add': 
+            return add(first, second)
+            break;
+        case 'subtract': 
+            return subtract(first, second)
+            break
+        case 'multiply':
+            return multiply(first, second)
+            break
+        case 'divide': 
+            return divide(first, second)
+            break
+        default: 
+            return 'error'
+    }
 }
-
 
 
 // write a function that accepts an array of many single digits
 // and outputs them as a string representing one number
+function arrToString(arr) {
+    return arr.join('').toString();
+} 
+
+  
+  
+  
+  
+  
--- calculator/index.js
+++ calculator/index.js
@@ -9,15 +9,27 @@
 // and a second array for the second number input
 // You'll want to track the "current array" in a global variable
 // and you may want to save the operation into a variable
+var first = [];
+var second = [];
+var current = first;
+var result = [];
+var op;
+var display = document.querySelector('output');
 
-
-
+let numbers = document.querySelector('.numbers');
+let operations = document.querySelector('.operations');
 
 // Listen for clicks on the buttons section element
     // check the event target to make sure it is a click on a valid button
     // push the values clicked into one of your arrays from above
     // update the output element to display the user input
-
+numbers.addEventListener('click', function(e) {
+    if(e.target.classList[0] == 'num'){
+        display.innerText = '';
+        current.push(e.target.classList[1]);
+        display.innerText = arrToString(current)
+    }
+})
 
 
 
@@ -31,3 +43,22 @@
         // display the result of your equalsHandler() function
         // reset the first and second arrays and the operation variable to be empty
     // Otherwise handle all other clicks by sending a message of "no valid selection" to the console
+operations.addEventListener('click', function(e) {
+    if(e.target.classList[0] == 'op') {
+        current = second
+        op = e.target.classList[1]
+        display.innerText = op
+    }else if(e.target.classList[0] == 'eq') {
+        current = first
+        let result = equalsHandler(op, arrToString(first), arrToString(second));
+        display.innerText = result;
+        first.length = 0
+        second.length = 0
+        op = ''
+    } else {
+        console.log('no valid selection')
+    }
+})
+    
+    
+    
