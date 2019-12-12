0fdc16ef1da64865e583639c738aa611de6abe9a
JamieVaughn <wxv6@case.edu>
2019-12-12 01:53:54 -0500

    Add calc challenge dir

---
+++ calculator/functions.js
@@ -0,0 +1,36 @@
+// Declare your utility functions in this file
+// See if you can create pure functions here 
+// which you would then call in your index.js file
+
+
+// Write a function to add two numbers
+
+
+
+// Write a function to subtract two numbers
+
+
+
+// Write a function to multiply two numbers
+
+
+
+// Write a function to divide two numbers
+
+
+
+
+// Write a function that accepts a string representing 
+// an operation and two numbers 
+// and returns a numerical result 
+// (use your functions from above within this function)
+function equalsHandler(operation, num1, num2){
+
+
+    return answer;
+}
+
+
+
+// write a function that accepts an array of many single digits
+// and outputs them as a string representing one number
---
+++ calculator/index.js
@@ -0,0 +1,33 @@
+// Create a basic calculator that can add, subtract, multiply & divide
+// Using Arrays, Functions and EventListeners will be crucial, 
+// but you may use any other techniques you would like
+// See below for an outline of tasks to guide you:
+
+
+// Declare your variables you need to save in the global scope up here
+// perhaps an array to contain the first number input 
+// and a second array for the second number input
+// You'll want to track the "current array" in a global variable
+// and you may want to save the operation into a variable
+
+
+
+
+// Listen for clicks on the buttons section element
+    // check the event target to make sure it is a click on a valid button
+    // push the values clicked into one of your arrays from above
+    // update the output element to display the user input
+
+
+
+
+// Listen for clicks on the operations section element
+    // check the event target to see if its a click on an 'op' button
+        // update the current array to the second array
+        // save the op value into the op variable
+        // upate the display to show the operation value
+    // check the event target to see if its a click on the 'eq' button
+        // update the current array to the first array
+        // display the result of your equalsHandler() function
+        // reset the first and second arrays and the operation variable to be empty
+    // Otherwise handle all other clicks by sending a message of "no valid selection" to the console
