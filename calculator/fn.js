3e66c5eb23f2ca2c1b59cd52711a3772f38dd08d
JamieVaughn <wxv6@case.edu>
2019-12-12 21:22:35 -0500

    add change

---
+++ calculator/fn.js
@@ -0,0 +1,6 @@
+function convertTemperature(temp) {
+    let bool = Boolean(temp.indexOf('F') + 1) // || Boolean(temp.indexOf('f') + 1) <- could check for lower case as well, but for simplicity, assume uppercase, also Would this work without using the Boolean() wrapper?
+    let index = bool ? temp.indexOf('F') : temp.indexOf('C')
+    let num = Number(temp.slice(0, index))
+    return bool ? (num - 32) / 1.8 : num * 1.8 + 32;
+  }
