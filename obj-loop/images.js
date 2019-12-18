22688a4a7971977dac764685f7429ad6d19e8ca6
JamieVaughn <wxv6@case.edu>
2019-12-17 19:04:06 -0500

    Add obj-loop challange

---
+++ obj-loop/images.js
@@ -0,0 +1,5 @@
+const nodeImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.iconfinder.com%2Fdata%2Ficons%2Fnodejs-1%2F256%2Fnodejs-256.png&f=1&nofb=1";
+const HTMLImg = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.w3.org%2Fhtml%2Flogo%2Fdownloads%2FHTML5_Logo_512.png&f=1&nofb=1";
+const CSSImg = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.logotypes101.com%2Flogos%2F194%2F830812341256B99B32E1A9F242BB9F5F%2Fcss3logo.png&f=1&nofb=1";
+const JSImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F9%2F99%2FUnofficial_JavaScript_logo_2.svg%2F240px-Unofficial_JavaScript_logo_2.svg.png&f=1&nofb=1";
+const ReactImg = "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdwglogo.com%2Fwp-content%2Fuploads%2F2017%2F09%2F1460px-React_logo-324x400.png&f=1&nofb=1";
---
+++ obj-loop/index.js
@@ -0,0 +1,21 @@
+// Create an array of Objects, each with a 
+// title property, a img property, a version property, and a learned property
+// We want to use this to keep track of our Web Development topics
+// So we want to track HTML, CSS, JS, React and Node as the topics
+const topics = [
+    {},
+    {},
+    {},
+    {},
+    {},
+]
+
+
+
+
+// Finish the function so it writes each of the Object properties into an HTML 
+// string template literal and then appends it to section#output as separate 
+// media Objects for each object. You can use the style.css as a guide for classNames
+function appendCards(node, obj) {
+
+}
