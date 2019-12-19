107c6fe2f669f8383d7abf7e4de6250a985fe8b2
JamieVaughn <wxv6@case.edu>
2019-12-19 13:36:42 -0500

    add starter code hints for obj looping

--- obj-loop/index.js
+++ obj-loop/index.js
@@ -3,19 +3,21 @@
 // We want to use this to keep track of our Web Development topics
 // So we want to track HTML, CSS, JS, React and Node as the topics
 const topics = [
-    {},
+    {
+        title: 'Javascript',
+        img: JSImg,
+        version: 'ES2019',
+        learned: true
+    },
     {},
     {},
     {},
     {},
 ]
 
-
-
-
 // Finish the function so it writes each of the Object properties into an HTML 
 // string template literal and then appends it to section#output as separate 
 // media Objects for each object. You can use the style.css as a guide for classNames
-function appendCards(node, obj) {
+function appendCards(el, array) {
 
 }
