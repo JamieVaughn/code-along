8b71dca77a77ca7ca1b8534881af2d5c0e9e2d44
JamieVaughn <wxv6@case.edu>
2024-12-18 12:30:30 -0500

    add rule 30 and cell wraparound logic

--- javascript/cellular-automata/script.js
+++ javascript/cellular-automata/script.js
@@ -8,17 +8,27 @@ const toggle = document.querySelector('.toggle')
 const step = document.querySelector('.step')
 const reset = document.querySelector('.reset')
 /* RULES: (8 conditions)
-  111   101   110   011   100   001   010   000
-  ◼◼◼ | ◼◻◼ | ◼◼◻ | ◻◼◼ | ◼◻◻ | ◻◻◼ | ◻◼◻ | ◻◻◻
-   ◻     ◻     ◼     ◼     ◼     ◼     ◻     ◻   <- output   */
+  111   110   101   100   011   010   001   000
+  ◼◼◼ | ◼◼◻ | ◼◻◼ | ◼◻◻ | ◻◼◼ | ◻◼◻ | ◻◻◼ | ◻◻◻
+   ◻     ◼     ◻     ◼     ◼     ◻     ◼     ◻   <- output   */
 const rule = {
   '111': 'dead', // 0,
-  '101': 'dead', // 0,
   '110': 'live', // 1,
-  '011': 'live', // 1,
+  '101': 'dead', // 0,
   '100': 'live', // 1,
-  '001': 'live', // 1,
+  '011': 'live', // 1,
   '010': 'dead', // 0,
+  '001': 'live', // 1,
+  '000': 'dead', // 0,
+}
+const rule30 = {
+  '111': 'dead', // 0,
+  '110': 'dead', // 0,
+  '101': 'dead', // 0,
+  '100': 'live', // 1,
+  '011': 'live', // 1,
+  '010': 'live', // 1,
+  '001': 'live', // 1,
   '000': 'dead', // 0,
 }
 // Loop over first row of cols to append a div w/ its calls for each cell
@@ -43,9 +53,12 @@ const calculateNewRow = () => {
   let cells = lastRowCellsAsBinary()
   const neighbors = []
   for(let i = 0; i < cells.length; i++) {
-    neighbors.push([cells[i-1] ?? 0, cells[i], cells[i+1] ?? 0])
+    let left = cells[i-1] ?? cells.at(-1) // ?? 0
+    let middle = cells[i]
+    let right = cells[i+1] ?? cells.at(0) // ?? 0
+    neighbors.push([left, middle, right])
   }
-  const state = neighbors.flatMap(group => rule[group.join('')])
+  const state = neighbors.flatMap(group => rule30[group.join('')])
   const nextRow = cells.map((_, index) => `<div class="${state[index]}"></div>`)
   grid.innerHTML += nextRow.join('')
 }
